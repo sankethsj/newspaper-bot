490e550a485ad1da1db4f61948f9a1fb5b11c7db
sankethsj <sankethjain81@gmail.com>
2024-09-15 09:54:33 +0900

    updated pdf handling and packages

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ def download_pdf(issue_id, date_string, page_no):
     mm = date_string[4:6]
     dd = date_string[6:8]
     page_no = str(page_no).zfill(2)
-    
+
     page_url = f"https://www.enewspapr.com/News/{issue}/{region}/{yyyy}/{mm}/{dd}/{date_string}_{page_no}.PDF"
 
     response = requests.get(page_url)
@@ -36,18 +36,24 @@ def download_pdf(issue_id, date_string, page_no):
 
     if response.status_code == 200:
         filename = page_url.rsplit("/", 1)[-1]
-        with open("tmp/" + filename, 'wb') as f:
+        with open("tmp/" + filename, "wb") as f:
             f.write(response.content)
     else:
         print("Error :", response.text)
 
 
 def export_to_single_df(issue_id, date_string):
-    
+
     merger = PdfWriter()
-    
+    file_found = False
     for pdf in sorted(os.listdir("tmp")):
+        file_found = True
         merger.append("tmp/" + pdf)
+
+    if not file_found:
+        merger.close()
+        print("E-paper not found")
+        return
 
     out_filename = f"output/{issue_id}_{date_string}.pdf"
     merger.write(out_filename)
@@ -57,8 +63,8 @@ def export_to_single_df(issue_id, date_string):
 
 if __name__ == "__main__":
 
-    current_time = dt.datetime.utcnow() + dt.timedelta(hours=5, minutes=30)
-    print("Current India time :",current_time)
+    current_time = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=5, minutes=30)
+    print("Current India time :", current_time)
 
     date_string = dt.datetime.strftime(current_time, "%Y%m%d")
     ISSUE_ID = "KANPRABHA_MN"
@@ -66,13 +72,13 @@ if __name__ == "__main__":
     print(f"Downloading '{ISSUE_ID}' of date : {date_string}")
 
     page_count = get_page_count(ISSUE_ID, date_string)
-    print("No. of pages :",page_count)
+    print("No. of pages :", page_count)
 
     os.makedirs("tmp", exist_ok=True)
     os.makedirs("output", exist_ok=True)
 
     pages = []
-    for i in range(1, page_count+1):
+    for i in range(1, page_count + 1):
         pages.append(i)
 
     print("Downloading all pages...")
@@ -93,12 +99,11 @@ if __name__ == "__main__":
     print("Complete")
 
     # delete pdf files older than 7 days
-    for file in os.listdir('output'):
+    for file in os.listdir("output"):
 
         file_date_str = file.split("_")[-1].split(".")[0]
         file_date = dt.datetime.strptime(file_date_str, "%Y%m%d")
 
         if (current_time - file_date).days > 7:
             print(f"Deleting File '{file}' older than 7 days")
-            os.remove('output/' + file)
-
+            os.remove("output/" + file)
